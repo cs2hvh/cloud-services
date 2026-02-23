823fb8f2b19b0e14d91583e656dbe9d0892166a5
deep <deep.aghera@ahurasense.com>
2026-02-23 14:15:46 +0530

    solution page added and created propely

---
+++ app/(marketing)/solutions/page.tsx
@@ -0,0 +1,9 @@
+import { SolutionsHeroSection } from "@/components/solutions/solutions-hero-section";
+
+export default function SolutionsPage() {
+  return (
+    <main className="bg-black">
+      <SolutionsHeroSection />
+    </main>
+  );
+}
